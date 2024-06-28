3ef57c219cc10b0552087c6e3a29870999038e24
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-27 18:10:24 -0600

    added courses component for dash board page

--- src/app/dashboard/[email]/page.tsx
+++ src/app/dashboard/[email]/page.tsx
@@ -1,7 +1,8 @@
+import Courses from '@/app/dashboard/courses';
 export default function Page() {
   return (
     <>
-      <h1>Here is where we will pick courses.</h1>
+      <Courses />
     </>
   );
 };
---
+++ src/app/dashboard/courses.tsx
@@ -0,0 +1,61 @@
+import { sql } from "@vercel/postgres";
+async function getCourses() {
+    'use server';
+
+    // Todo - get courses from the database
+    // for now just return a list of courses
+    return [
+        {
+            id: 1,
+            name: 'Software Development Diploma',
+            description: 'Learn software development',
+            duration: '4 terms',
+            level: 'Diploma',
+            department: 'School of technology'
+        },
+        {
+            id: 2,
+            name: 'Software Development Apprenticeship',
+            description: 'Learn software development',
+            duration: '4 terms',
+            level: 'Apprenticeship',
+            department: 'School of technology'
+        },
+        {
+            id: 3,
+            name: 'Software Development Certificate',
+            description: 'Learn software development',
+            duration: '4 terms',
+            level: 'Certificate',
+            department: 'School of technology'
+        },
+        {
+            id: 4,
+            name: 'Software Development Degree',
+            description: 'Learn software development',
+            duration: '4 terms',
+            level: 'Degree',
+            department: 'School of technology'
+        }
+    ];
+}
+
+export default async function Courses() {
+    const courses = await getCourses();
+    return (
+        <div className="h-200 w-96 bg-blue-200 rounded-lg shadow-md p-4" >
+            <h1 className="text-2xl font-bold mb-4">Available Courses</h1>
+            <ul>
+                {courses.map((course) => (
+                    <li key={course.id} className="mb-4">
+                        <h2 className="text-xl font-bold">{course.name}</h2>
+                        <p>{course.description}</p>
+                        <p>{course.duration}</p>
+                        <p>{course.level}</p>
+                        <p>{course.department}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
