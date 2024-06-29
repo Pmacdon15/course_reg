d31866d268c6c8c012bf060cf18d27611e1225c9
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-06-28 22:26:12 -0600

    css tweaks

--- src/app/dashboard/userCourses.tsx
+++ src/app/dashboard/userCourses.tsx
@@ -3,8 +3,9 @@ import { sql } from "@vercel/postgres";
 type UserCourse = {
     id: number;
     name: string;
-    description: string;
-    duration: string;
+    brief_description: string;
+    school: string;
+    terms: string;
     registered: boolean;
 }
 
@@ -12,6 +13,9 @@ async function getUserCourses(email: string) {
     const results = await sql`
         SELECT 
         AC.name, 
+        AC.brief_description,
+        AC.school,
+        AC.terms,
         CC.registered
         FROM 
         CRAvailableCourses AC
@@ -31,8 +35,8 @@ export default async function UserCourses({ email }: { email: string }) {
     const userCourses = await getUserCourses(email);
     console.log(userCourses);
     return (
-        <>        <div className="md:h-4/6 h-min w-full bg-gradient-to-r from-blue-400 to-blue-200 rounded-lg shadow-md gap- p-4">
-            <div className="text-center">
+        <div className="md:h-4/6 h-min w-full text-center content-center bg-gradient-to-r from-blue-400 to-blue-200 rounded-lg shadow-md gap- p-4">
+            <div >
                 <h2 className="text-xl font-bold">Your Current Courses:</h2>
                 {userCourses.length === 0 ? (
                     <p>No current courses to list</p>
@@ -40,14 +44,17 @@ export default async function UserCourses({ email }: { email: string }) {
                     userCourses.filter((course) => course.registered).map((course) => (
                         <div key={(course.id)} className="mb-4">
                             <h4>{course.name}</h4>
+                            <p>To {course.brief_description}</p>
+                            <p>at our {course.school}</p>
+                            <p>over {course.terms} terms</p>
                         </div>
                     ))
                 )}
             </div>
-            <div className="text-center">
+            <div >
                 <h2 className="text-xl font-bold">Past Courses:</h2>
                 {userCourses.filter((course) => !course.registered).length === 0 ? (
-                    <p>No past courses to list</p>
+                    <p>No past courses to list.</p>
                 ) : (
                     userCourses.filter((course) => !course.registered).map((course) => (
                         <div key={(course.id)} className="mb-4">
@@ -57,7 +64,5 @@ export default async function UserCourses({ email }: { email: string }) {
                 )}
             </div >
         </div >
-        </>
-
     );
 };
