16f37a141eb28879c90b54376678b9645fa52e2e
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-07-05 13:50:18 -0600

    fixed component to deal with more words in the class name

--- src/app/register/buttonClassInfo.tsx
+++ src/app/register/buttonClassInfo.tsx
@@ -1,17 +1,20 @@
 'use client';
 import { useRouter } from 'next/navigation';
+
 export default function ButtonCourseInfo({ className }: { className: string }) {
     const router = useRouter();
+    const words = className.split(" ");
+    const firstWord = words[0]?.toLowerCase() || '';
+    const restOfWords = words.slice(1).map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+
+    const path = `classes/${firstWord}${restOfWords}`;
     return (
         <h2 className='text-md font-bold'>
             <button className='focus:outline-none text-left' onClick={() => {
-                const [firstWord, secondWord, thirdWord] = className.split(" ");
-                const path = thirdWord ? `classes/${firstWord.toLowerCase()}${secondWord.toLowerCase()}${thirdWord.toLowerCase()}` : `classes/${firstWord.toLowerCase()}${secondWord.toLowerCase()}`;
                 router.push(`/${path}`);
-            }} >
+            }}>
                 {className}
             </button>
         </h2>
     );
-
-};
+}
